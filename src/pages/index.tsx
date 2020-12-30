13938321ca5ec461930d24fa9c3e4a686b181146
Gabriel Campos <caampos22@gmail.com>
2020-12-30 18:32:20 -0300

    Improve on styles and add SEO component
    * add first version of the website
    
    * add SEO component and improve style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import { Header, Grid } from '@/containers'
+import { SEO } from '@/components'
 
 export default function Home() {
   return (
     <>
+      <SEO 
+        title='Home'
+        description='A technology enthusiast passionate about JavaScript and OpenSource'
+        image='/assets/me.jpeg'
+      />
       <Header />
       <Grid />
     </>

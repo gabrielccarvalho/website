f755ec8bf6a9e88cb68e6610bebb0008adb14d32
Gabriel Campos <caampos22@gmail.com>
2020-12-30 14:42:45 -0300

    add first version of the website

---
+++ src/containers/Header.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Image from 'next/image'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 0;
+`
+
+const ProfilePicture = styled(Image).attrs({ 
+  src: '/assets/me.jpeg',
+  alt: 'Picture of the author',
+  width: 200,
+  height: 200,
+  layout: 'fixed'
+  })`
+  border-radius: 50%;
+`
+
+const Name = styled.h1`
+  margin: 2rem 0 1rem 0;
+  font-size: 3.5rem;
+  line-height: 1.15;
+  text-align: center;
+  letter-spacing: -0.02em;
+`
+
+const Description = styled.p`
+  margin: 0;
+  text-align: center;
+  line-height: 1.5;
+  font-size: 1.3rem;
+`
+
+const Header = () => {
+
+  return (
+    <Container>
+      <ProfilePicture />
+      <Name> Gabriel Campos </Name>
+      <Description> A technology enthusiast passionate about JavaScript and OpenSource </Description>
+    </Container>
+  )
+}
+
+export default Header
